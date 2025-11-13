b00354eac2593698a75361f9b43362043d5a3492
Nikita Musalov <musalov@eesee.io>
2025-11-13 10:56:00 +0300

    feat: refine

--- src/lc/calculi_vanilla.pxd
+++ src/lc/calculi_vanilla.pxd
@@ -1,25 +1,28 @@
-# distutils: language=c++
 from libcpp.unordered_set cimport unordered_set
 from libcpp.string cimport string as cpp_string
 
+
 cdef class Term:
     cdef public unordered_set[cpp_string] free_vars
-    cpdef Term subst(self, str var, Term value)
+    cpdef Term subst(self, cpp_string var, Term value)
     cpdef Term reduce(self)
 
+
 cdef class Var(Term):
-    cdef public str name
-    cpdef Term subst(self, str var, Term value)
+    cdef public cpp_string name
+    cpdef Term subst(self, cpp_string var, Term value)
     cpdef Term reduce(self)
+
 
 cdef class App(Term):
     cdef public Term func
     cdef public Term arg
-    cpdef Term subst(self, str var, Term value)
+    cpdef Term subst(self, cpp_string var, Term value)
     cpdef Term reduce(self)
 
+
 cdef class Abs(Term):
-    cdef public str param
+    cdef public cpp_string param
     cdef public Term body
-    cpdef Term subst(self, str var, Term value)
+    cpdef Term subst(self, cpp_string var, Term value)
     cpdef Term reduce(self)
--- src/lc/calculi_vanilla.pyx
+++ src/lc/calculi_vanilla.pyx
@@ -1,12 +1,12 @@
-# distutils: language=c++
 from __future__ import annotations
 import re
 from ..common.utils import get_random_var_name
 from libcpp.unordered_set cimport unordered_set
 from libcpp.string cimport string as cpp_string
 
+
 cdef class Term:
-    cpdef Term subst(self, str var, Term value):
+    cpdef Term subst(self, cpp_string var, Term value):
         raise NotImplementedError("Subclasses must implement this method")
 
     cpdef Term reduce(self):
@@ -29,11 +29,11 @@ cdef class Term:
 
 cdef class Var(Term):
     def __init__(self, str name):
-        self.name = name
+        self.name = cpp_string(name.encode())
         self.free_vars = unordered_set[cpp_string]()
-        self.free_vars.insert(cpp_string(name.encode()))
+        self.free_vars.insert(self.name)
 
-    cpdef Term subst(self, str var, Term value):
+    cpdef Term subst(self, cpp_string var, Term value):
         if self.name == var:
             return value
         return self
@@ -42,31 +42,28 @@ cdef class Var(Term):
         return self
 
     def __eq__(self, other):
-        return isinstance(other, Var) and self.name == other.name
+        return isinstance(other, Var) and self.name == (<Var>other).name
 
     def __str__(self) -> str:
-        return self.name
+        return self.name.decode('utf-8')
 
     def __repr__(self) -> str:
-        return self.name
+        return self.name.decode('utf-8')
 
 
 cdef class App(Term):
     def __init__(self, Term func, Term arg):
-        cdef cpp_string cpp_var
         self.func = func
         self.arg = arg
         self.free_vars = unordered_set[cpp_string]()
 
-        # Copy all elements from func.free_vars
+        cdef cpp_string cpp_var
         for cpp_var in func.free_vars:
             self.free_vars.insert(cpp_var)
-
-        # Copy all elements from arg.free_vars
         for cpp_var in arg.free_vars:
             self.free_vars.insert(cpp_var)
 
-    cpdef Term subst(self, str var, Term value):
+    cpdef Term subst(self, cpp_string var, Term value):
         return App(
             self.func.subst(var, value),
             self.arg.subst(var, value)
@@ -107,31 +104,28 @@ cdef class App(Term):
 
 cdef class Abs(Term):
     def __init__(self, str param, Term body):
-        cdef cpp_string cpp_var
-        cdef cpp_string cpp_param = cpp_string(param.encode())
-        self.param = param
+        self.param = cpp_string(param.encode())
         self.body = body
         self.free_vars = unordered_set[cpp_string]()
 
-        # Copy all elements from body.free_vars except param
+        cdef cpp_string cpp_var
         for cpp_var in body.free_vars:
-            if cpp_var != cpp_param:
+            if cpp_var != self.param:
                 self.free_vars.insert(cpp_var)
 
-    cpdef Term subst(self, str var, Term value):
-        cdef cpp_string cpp_var = cpp_string(var.encode())
-        cdef cpp_string cpp_param = cpp_string(self.param.encode())
+    cpdef Term subst(self, cpp_string var, Term value):
         if self.param == var:
             return self
-        elif self.body.free_vars.count(cpp_var) == 0 and value.free_vars.count(cpp_param) == 0:
+        elif self.body.free_vars.count(var) == 0 and value.free_vars.count(self.param) == 0:
             return self
-        elif self.body.free_vars.count(cpp_var) == 0 or value.free_vars.count(cpp_param) == 0:
+        elif self.body.free_vars.count(var) == 0 or value.free_vars.count(self.param) == 0:
             return Abs(
-                self.param,
+                self.param.decode('utf-8'),
                 self.body.subst(var, value)
             )
 
         cdef str new_variable = get_random_var_name()
+        cdef cpp_string cpp_new_variable = cpp_string(new_variable.encode())
         cdef Term alpha_step = self.body.subst(self.param, Var(new_variable))
         cdef Term subst_step = alpha_step.subst(var, value)
         return Abs(
@@ -140,16 +134,16 @@ cdef class Abs(Term):
         )
 
     cpdef Term reduce(self):
-        return Abs(self.param, self.body.reduce())
+        return Abs(self.param.decode('utf-8'), self.body.reduce())
 
     def __eq__(self, other):
-        return isinstance(other, Abs) and self.param == other.param and self.body == other.body
+        return isinstance(other, Abs) and self.param == (<Abs>other).param and self.body == other.body
 
     def __str__(self) -> str:
         cdef str body_str = f"({self.body})"
         if isinstance(self.body, (App, Abs)) or isinstance(self.body, Var):
             body_str = re.sub(r"^\((.*)\)$", r"\1", body_str)
-        return f"\\{self.param}. {body_str}"
+        return f"\\{self.param.decode('utf-8')}. {body_str}"
 
     def __repr__(self) -> str:
-        return f"(\\{self.param}. {repr(self.body)})"
+        return f"(\\{self.param.decode('utf-8')}. {repr(self.body)})"
